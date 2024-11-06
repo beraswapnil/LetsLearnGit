e49283bb2e6887c1e54fa8f376c441bcb6b49694
Swapnil Bera <beraswapnil47@gmail.com>
2024-11-06 15:10:46 +0530

    Add two files

---
+++ Multiply.java
@@ -0,0 +1,6 @@
+public class Multiply {
+    // Method to multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,6 @@
+public class Sum {
+    // Method to calculate the sum of two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
