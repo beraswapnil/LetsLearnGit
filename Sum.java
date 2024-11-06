c9950458533fdf8a2acb52557efe511f623bc2b5
Swapnil Bera <beraswapnil47@gmail.com>
2024-11-06 15:16:54 +0530

    Fix previous commit: sum.add method was not modified

--- Sum.java
+++ Sum.java
@@ -3,4 +3,13 @@ public class Sum {
     public int add(int a, int b) {
         return a + b;
     }
+
+    // Method to calculate the sum of an array of numbers
+    public int add(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
