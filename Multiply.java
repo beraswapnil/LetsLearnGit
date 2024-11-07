6ca3b0ab9e5edda4c1c6d0336c72bd10ba2bf7b7
Swapnil Bera <beraswapnil47@gmail.com>
2024-11-07 19:21:38 +0530

    Modified Multiply.java file

--- Multiply.java
+++ Multiply.java
@@ -3,4 +3,9 @@ public class Multiply {
     public int multiply(int a, int b) {
         return a * b;
     }
+
+    // Overloaded method to multiply three numbers
+    public int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
 }
